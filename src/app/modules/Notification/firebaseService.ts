8eb46871957728767b671328bb40dde431ea7600
sahin siraj <sahinsiraj360@gmail.com>
2025-06-23 23:32:53 +0600

    add firebase secret

--- src/app/modules/Notification/firebaseService.ts
+++ src/app/modules/Notification/firebaseService.ts
@@ -1,44 +1,44 @@
-export const serviceAccount = {
-    type: "service_account",
-    project_id: "learner-36e02",
-    private_key_id: "d16f95f53126c6d60fbfff653621c1448d1e18a3",
-    private_key:`-----BEGIN PRIVATE KEY-----
-    MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC6rxrUbfTeHeFj
-    XPSeoDuHOUF4bq90MVXisroGo7Y1RH+MTey+SZIGXEU74RZnngePbQduRd907QUk
-    qiPYAf/CUC71xnVN3xkI8JLVFqmdbvfsZX2Ck4q0biK7UtysvRi115wzISuOVvBg
-    DhFCHpS/X48ezQ0HGFphDLNDNxi8bJvKe/9OpqIYwml9qHvayvWa4TGsil15OGXG
-    PLV32KNmcPT0hYwijteORizVqelP0+ZzGSFEqli+UOBgXH6xXPzVSI5fpRaQRmMr
-    sFt0AVVbJqSacOwe9N0xpxsxdCHQcMNSBCf6UyGKcWKMuaeCrXmnhLDLnXFp4Bsb
-    qeCDX5rFAgMBAAECggEABWtfkFdhQoebCPcWnCgeukfU+0QCd2zNXk05UBDQ9hBp
-    ZH9aw+Qfgsev4dgXa87/PUK0xOkTlW0iMqAbY6IcpuIMYTv0Dnfqy5nkkwwfxMY0
-    4hdtcpXuaaRvAS9bqbg8UYNg9hIFw6P3HRfjc98KL6Izbubv62rqv1UBBIWL+uSV
-    T/DHax0UBgOKvLh774u9uJHnIOmFxH065w+7KmQ/M0xVvT7bstpVK72yaH/IQixs
-    68qFLieDWnYIgaOw/o3CLgi9ezYRx6zI68iVr7hCGj/bxc0nIShQbKYY6O3x0/Dw
-    KRBQmvQgntNHmXcsMmNzzLPcZNl4pmF+ADkTs3Po0QKBgQD59Z0Db0rqYrzz0Okq
-    qtFuc0Xna2TPcDYUKTmuIAqvchB6RFg9QsdWsAZ0t18oLPDHaoXQbXtqqp6kkWB7
-    uNz/tBu/5Hm9VI8XjWmu3gS8AxNCOu7jqHSWtkqCdEf8W6HNO90FTpoq0gQBjlKy
-    2qXXH04HI6g5f/uIqm8zjL+DdQKBgQC/MgjsGH9clYp9FVcznoS/y4xHiQ1R596u
-    NQiwH66PVVXFqM51IdBaJiTxA7SrUKtpUYNtbgFvJlBkDm4uC0oeusJGCXiiOMLt
-    Lb86Izn3c486EiXFC+B3yuCYHseY76qrhbf1cowwgTR3attufV/Th02J9DZak8WB
-    Vsh01GxgEQKBgQDm0he/PzuX+7vvDsDyZLFnZE1uKa77G+zkazlXgUE8yIFM1uYB
-    FDSN6ZjcmSTnLbDfBZAya5dnZc1/vLuzJH/4JPLnHe4JlxdYMX8hOuZ2VASpyVAm
-    rL2VGxrtIsIC2P8uTZSJPQ+CcVO/uBTIEr7IS6E7WIMVLgkyJ+juWo/TrQKBgQCl
-    mrQJs2pVih1rHFnslwgKjEbUC0yjPgAtyjfdPYPYGKTzULSK1h/+hX2H2JrbrELc
-    NUNuPFM3r+r/z1P1w8eOeFsfiMKZWCrWfsJcm/hwcgKD3QYj95aUjAAfIsHloOmm
-    KWteSLpOpQsJyF+KA0RGFrK9o+6UY9w1nDKCpx+70QKBgBsXEVK5BLvCslQHQ7yr
-    ervK0z7sKxeDk46s6dQJVsnFQuDua9tYKWojCIRcI/T7xye/ozXowUcl+J4VQWn4
-    ++E9+oR8yvYKv4Il4W2pPm/lyKB1gPxxT4mZANdxpbVr9UFELB3crP7EcQlEQuAs
-    oP4YksBMytrwBXGA+ZgAsnRz
-    -----END PRIVATE KEY-----`,
-    client_email: "firebase-adminsdk-fbsvc@learner-36e02.iam.gserviceaccount.com",
-    client_id: "114146541996701284366",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40notifications-3428c.iam.gserviceaccount.com",
-    universe_domain: "googleapis.com",
-  };
-  
+
+  export const serviceAccount = {
+  type: "service_account",
+  project_id: "mindset-minute-app-523ff",
+  private_key_id: "7fc1d04a282c2aca84b51869e125747e758185e2",
+  private_key: `-----BEGIN PRIVATE KEY-----
+  MIIEugIBADANBgkqhkiG9w0BAQEFAASCBKQwggSgAgEAAoIBAQCz5wuKPPM85HGV
+  y74ZqM4jW7tcYVo7LrOxCCe2qE70xJHHq0nZHJbczFHIL+AUtFutkYhvR/sbAJHH
+  AwlwqNYccuNe0C7n1YP48jMtBYzP3Kdzzf468Vt7d5lrao/p71vS6VrQQaVyMRA2
+  MVChnUm4HmSboGqqle6MldK0t2BokHcdQIsRyCQ+PexAINaw68mcKUZTCSR5Ju3P
+  IkYuVx9Jm1YFh2hhY3FllRX7oiO9H3uMW3D3TWlOevXtLCJ3LwuCK/IuhyZjWgbe
+  fym80sMCTfRPkNSuL7syqgCXf4xnOaq51gyT93+V3GK7K0g5/VJusTKrPDVQSBAT
+  HSP5EItVAgMBAAECggEAEWb6n0spDk1czWwxfeLt2c7AT33dUBdC64+J4+nhBV1U
+  arefg6B32XDsQkybRPVzXtVSmM0iNaj28QTSr3Ktj3/vjZbeyq9FPloTaDn+toa2
+  LgYRnxsEyes2TIU42Zy9SagoEDghbGvhkRRxI8kGvgg2nDVGzCyHsFK32KJacZLK
+  ZegVQiePFz+n/5SJ+DDJy0npVgAh6K0v2FgdrPB4ND+Jm5jLFsS81VcHlFbnMbk5
+  rTFPDo1bk++bLwqJ/rD3FRE1jMwcaprtmXsEInH84MQxWF0i3D83bHsaW4l+SI/6
+  F3D7tALFFDPkkOzWtmqrJeT2wpRTN3aUrFd4ndiCQQKBgQDYAH5hwI7JY0cynmyj
+  h/Qf1cTuA4qZ6BPcoB7RzzNlUynhvvfU35Ud9DiHuPouhom6a6Ca/jCzniFcfGsN
+  FRc/LXdbh+2DGmTIPoB3xFz8stHsbavOoNAXudBCFttkAtG3sdyJlGguWY0pv4uD
+  9ySEKvEWtKY0GtJ0/eg+08dvYQKBgQDVN0USgx+ALQJtBJjmWD55Ic7TmXmT9pHy
+  FqUKkbtk+HPXsKXMgN0urA/alk67qgT4QJJIUdBnNx3Zil4Q6WTBNFPjwmqqTXyt
+  O/b1G1MiXt344NhviLdr60Voi1kyQ2dWX57Eo5duML3BuQnYCbD8maNKZoVLf+Qg
+  BcuPn+EkdQKBgFksWwCgLlKV2d7b6J3akr5R0mswqxe+5/Q9ZnvXvrAmFTRNUqNf
+  aoBjQrNFeoOap2F+7V8Pcau1rLAx7UAJZP+AFbaD91oYm2LP6tmHB5yLdn7VCAjV
+  +xLQc1nVdd1kDJB5rUJfMmNwNQb3PPHOJhUqZT2qdrMsH5aM/ERzyfIBAn8nJP6J
+  Z4lg/uJzIWTw5UKS+qH+Ny8a+ynQSFgEmLzOzM53yvallQMGjpBakT3jNlOzQCeb
+  H2ojiPrx9XVPpYnZ7sF4DzmGCrPZGik81sdsh/44sw8JaxBBv+/+QZceo0FVmjiL
+  4XzrvgcYieBEyYK03YAdKLVQJyq6tlWpCLFNAoGAc4KBt7yvGBP3NAOeWhyKqbQc
+  ujF3vT1dyaQnwvT7iND/z0oGsT0+T4pzQFOHOiLD12br5t0/Xv5YaLlzs+Yhv1OH
+  iWgy8gnhY0LHjGEJf/onZghgxOfeQsx1kUPuSf+p3t5bADKmMdZdBTqaknZc2d17
+  SqlbQYGEtvnMLKnNw6A=
+  -----END PRIVATE KEY-----`,
+  client_email: "firebase-adminsdk-fbsvc@mindset-minute-app-523ff.iam.gserviceaccount.com",
+  client_id: "112586995014394272568",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40mindset-minute-app-523ff.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
+}
 
 //   {
 //   "type": "service_account",
